541e3c03e577bd48a5c909bf385981afd7b20f11
givara <givoo25@gmail.com>
2019-12-17 15:32:16 +0100

    Updates

---
+++ precache-manifest.e1eddd7f2dad5457744e6527b298160e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49346913131b0abc712343ad87cd24fc",
+    "url": "/React-Fetch/index.html"
+  },
+  {
+    "revision": "da8b88899dcda94891fb",
+    "url": "/React-Fetch/static/css/main.7cc3caa5.chunk.css"
+  },
+  {
+    "revision": "b2cd8fa03f7d5b80ad1c",
+    "url": "/React-Fetch/static/js/2.843fec27.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-Fetch/static/js/2.843fec27.chunk.js.LICENSE"
+  },
+  {
+    "revision": "da8b88899dcda94891fb",
+    "url": "/React-Fetch/static/js/main.55cca224.chunk.js"
+  },
+  {
+    "revision": "58cf85d6ba0533eadbd8",
+    "url": "/React-Fetch/static/js/runtime-main.4e4b8543.js"
+  }
+]);
